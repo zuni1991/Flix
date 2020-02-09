92095f40e7d580ca0ff41f76b273affd5f9d419c
Juan Zuniga <jzunigaruiz@csumb.edu>
2020-02-09 00:36:12 -0800

    Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Juan Zuniga on 2/8/20.
+//  Copyright © 2020 Juan Zuniga. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
